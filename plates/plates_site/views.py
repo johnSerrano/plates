9991f77928293d6a36cf5f4bdef4d7244ee472a9
John Serrano <johnserrano404@gmail.com>
2016-07-20 21:42:04 -0700

    created search page

--- plates/plates_site/views.py
+++ plates/plates_site/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 def index(request):
 	return render(request, "plates_site/index.html")
+
+def search(request):
+	return render(request, "plates_site/search.html")
